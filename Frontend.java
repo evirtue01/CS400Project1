871402b4305f350bf4274fec1be8da838a5d9349
alonergan <79943132+alonergan@users.noreply.github.com>
2021-03-02 22:14:46 -0600

    Add files via upload
    Still not completely finished. Need to get backend working first

--- Frontend.java
+++ Frontend.java
@@ -1,11 +1,11 @@
 // --== CS400 File Header Information ==--
-// Name: <the name of the team member who wrote the code in this file>
-// Email: <the team member's @wisc.edu email address>
-// Team: <the team name: two letters and 'red', 'blue', or 'purple'>
-// Role: <the team member's role in your team>
-// TA: <name of the team's ta>
-// Lecturer: <name of the team mate's lecturer>
-// Notes to Grader: <optional extra notes>
+// Name: Aidan Lonergan
+// Email: alonergan@wisc.edu
+// Team: blue
+// Role: Front End Developer
+// TA: Sid
+// Lecturer: Gary Dahl
+// Notes to Grader: Implementation is not currently finished
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -16,7 +16,6 @@ import java.io.IOException;
 
 
 public class Frontend {
-    private static List<String> ratings = new ArrayList<String>();
     
     private static String baseHeader =
         "=======================\n"
@@ -34,9 +33,16 @@ public class Frontend {
       + " Rating Selection Mode \n"
       + "=======================\n"
       + "Selected ratings are marked with a [x]\n";
+    
+    private static String detailsHeader =
+        "=======================\n"
+      + "     Movie Details     \n"
+      + "=======================\n";        
 
     private static Backend b;
-
+    
+    private static List<String> allRatings;
+    
     /**
      * Main method for running the program
      * @param args      
@@ -46,6 +52,11 @@ public class Frontend {
         Scanner scnr = new Scanner(System.in);
         boolean done = false;
         BufferedReader r;
+        // populate allRatings list
+        allRatings = new ArrayList<String>();
+        for (int i = 0; i < 10; i++) {
+            allRatings.add(i + ".0 - " + i + ".99");
+        }
 
         try {
             r = new BufferedReader(new FileReader("src/movies.csv"));
@@ -56,12 +67,21 @@ public class Frontend {
                 printBaseMenu();
                 String input = scnr.next();
                 switch (input) {
-                    case "1":
-                        
+                    case "1":                
                     case "2":
-
                     case "3":
-
+                        // get first three movies at index of input
+                        // the first element will be the movie selected
+                        List<MovieInterface> movies = b.getThreeMovies((Integer.parseInt(input) - 1));
+                        String movieDetails = movies.get(0).toString();
+                        while (!input.contentEquals("x")) {
+                            // print details
+                            System.out.print(detailsHeader);
+                            System.out.println(movieDetails);
+                            System.out.println("Enter 'x' to exit to return to base mode");
+                            input = scnr.next();                            
+                        }
+                        break;
                     case "g":
                         printGenreMenu();
                         input = scnr.next();
@@ -86,7 +106,23 @@ public class Frontend {
                         break;
                     case "r":
                         printRatingsMenu();
-                        
+                        input = scnr.next();
+                        while (!input.contentEquals("x")) {
+                            List<String> selRatings = b.getAvgRatings();
+                            int i = (int) Double.parseDouble(input);
+                            String currRating = allRatings.get(i);
+                            // loop through selected genre list
+                            for (int j = 0; j < selRatings.size(); j++) {
+                                // if in the selected list deselect otherwise add to selected list
+                                if (1 > 2 ) { // FIXME
+                                    b.removeAvgRating(input);
+                                } else {
+                                    b.addAvgRating(input);
+                                }
+                            }
+                            printGenreMenu();
+                            input = scnr.next();   
+                        }
                         break;
                     case "x":
                         System.out.println("Goodbye!");
@@ -170,20 +206,22 @@ public class Frontend {
         System.out.print(ratingsHeader);
         
         // print selected ratings
-        List<String> allRatings = new ArrayList<String>();
-        for (int i = 0; i < 10; i++) {
-            allRatings.add(i + ".0 - " + i + ".99");
-        }
         List<String> selRatings = b.getAvgRatings();
         for (int i = 0; i < allRatings.size(); i++) {
             for (int j = 0; j < selRatings.size(); j++) {
-                if (//TODO) {
+                String selected = selRatings.get(j);
+                int selRating = (int) Double.parseDouble(selected);
+                if (selRating >= i && selRating <= (i + .99)) {
                     System.out.println((i + 1) + ". [x] " + allRatings.get(i));                   
                 } else {
                     System.out.println((i + 1) + ". [ ] " + allRatings.get(i));
                 }
             }
-        }         
+        }
+        
+        // print commands
+        System.out.println("Enter 'x' to return to base mode");
+        System.out.println("Enter a number to select/deselect ratings");
         
     }
 }
